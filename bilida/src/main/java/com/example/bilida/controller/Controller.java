1554be7794b5d2bb7c8ab175e8d1b900c238d0f9
Tim <Tim@Roku>
2019-04-26 11:55:39 -0400

    change whatever

--- bilida/src/main/java/com/example/bilida/controller/Controller.java
+++ bilida/src/main/java/com/example/bilida/controller/Controller.java
@@ -5,10 +5,14 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.example.bilida.model.User;
 import com.example.bilida.service.Service;
 
 //import Configuration.BasicConfiguration;
@@ -24,19 +28,31 @@ public class Controller {
 
 	@GetMapping("/welcome")
 	public String welcome() {
-		System.out.println("entered mapping /welcome");
+		System.out.println("entered mapping /welcomeVV");
 		return service.retrieveWelcomeMessage();
 	}
 
 	@GetMapping("/dynamic-configuration")
+	//@ResponseBody
 	public Map<String, Object> dynamicConfiguration() {
 		Map<String, Object> map = new HashMap<String, Object>();
-		map.put("message", "message");
-		map.put("number", "number");
+		map.put("message", "Tim");
+		map.put("number", "10");
 		map.put("value", "value");
 		return map;
 	}
 	
+	@GetMapping("/requestResponse")
+	//@ResponseBody
+	public Map<String, Object> requestResponse(@RequestBody User user) {
+
+		Map<String, Object> map = new HashMap<String, Object>();
+
+		map.put("message", user.getMessage());
+		map.put("number", user.getNumber());
+		map.put("value", user.getValue());
+		return map;
+	}
 
 	@GetMapping("/welcome/{name}")
 	public String welcome(@PathVariable String name) {
---
+++ bilida/src/main/java/com/example/bilida/model/User.java
@@ -0,0 +1,47 @@
+package com.example.bilida.model;
+
+
+import java.util.List;
+
+public class User {
+	private String message;
+	private String number;
+	private String value;
+
+
+	public User() {
+
+	}
+	
+	public User(String message, String number, String value) {
+
+		this.message = message;
+		this.number = number;
+		this.value = value;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getNumber() {
+		return number;
+	}
+
+	public void setNumber(String number) {
+		this.number = number;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+}
