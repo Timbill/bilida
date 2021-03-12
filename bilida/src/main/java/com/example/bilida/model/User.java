1e3614d98f3b7a426a95cfebeb9201eda67f0a45
Tim <tbil99@netscape.net>
2021-03-12 17:45:31 -0500

    Lombok changes

--- bilida/src/main/java/com/example/bilida/model/User.java
+++ bilida/src/main/java/com/example/bilida/model/User.java
@@ -1,8 +1,14 @@
 package com.example.bilida.model;
 
+import lombok.Getter;
+import lombok.Setter;
 import lombok.Data;
+
 import java.util.List;
 //@Data
+
+@Getter
+@Setter
 public class User {
 	private String message;
 	private String number;
@@ -13,35 +19,24 @@ public class User {
 
 	}
 	
-	public User(String message, String number, String value) {
+/*	public User(String message, String number, String value) {
 
 		this.message = message;
 		this.number = number;
 		this.value = value;
 	}
-
-	public String getMessage() {
-		return message;
-	}
-
-	public void setMessage(String message) {
-		this.message = message;
-	}
-
-	public String getNumber() {
-		return number;
-	}
-
-	public void setNumber(String number) {
-		this.number = number;
-	}
-
-	public String getValue() {
-		return value;
-	}
-
-	public void setValue(String value) {
-		this.value = value;
-	}
-
+*/
+	/*
+	 * public String getMessage() { return message; }
+	 * 
+	 * public void setMessage(String message) { this.message = message; }
+	 * 
+	 * public String getNumber() { return number; }
+	 * 
+	 * public void setNumber(String number) { this.number = number; }
+	 * 
+	 * public String getValue() { return value; }
+	 * 
+	 * public void setValue(String value) { this.value = value; }
+	 */
 }
